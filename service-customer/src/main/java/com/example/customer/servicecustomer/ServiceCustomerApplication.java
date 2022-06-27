35abd6e4cf616ec7cbabb7f7f5bb5ce147b692f2
Vicente <vicente.mori@unmsm.edu.pe>
2022-06-27 12:37:44 -0500

    update product and shopping service

--- service-customer/src/main/java/com/example/customer/servicecustomer/ServiceCustomerApplication.java
+++ service-customer/src/main/java/com/example/customer/servicecustomer/ServiceCustomerApplication.java
@@ -2,6 +2,9 @@ package com.example.customer.servicecustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceCustomerApplication {
 
